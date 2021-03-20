e6c4754c95909efc2598da6be89f797a71e0759e
itheima <itheima@itcast.cn>
2021-03-20 19:58:19 +0800

    init project

---
+++ assets/js/login.js
@@ -0,0 +1,37 @@
+$(function() {
+    $('#link_reg').on('click', function() {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    $("#link_login").on('click', function() {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+
+    var form = layui.form
+    form.verify({
+        pwd: [/^[\S]{6,12}$/,
+            '密码必须6到12位,且不能出现空格'
+        ],
+        repwd: function(value) {
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致!'
+            }
+        }
+    })
+
+    $('#form_reg').on('submit', function(e) {
+        e.preventDefault()
+        $.post('http://api-breakingnews-web.itheima.net', {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }, function(res) {
+            if (res.status !== 0) {
+                return console.log(res.message)
+            }
+            console.log('注册成功！')
+        })
+    })
+})
