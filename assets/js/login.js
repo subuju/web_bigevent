c0ed03cb4c576a9800e324bb6a193332d9367e4e
itheima <itheima@itcast.cn>
2021-03-20 20:49:03 +0800

    完成了登录和注册功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,28 +10,51 @@ $(function() {
     })
 
     var form = layui.form
+    var layer = layui.layer
     form.verify({
-        pwd: [/^[\S]{6,12}$/,
-            '密码必须6到12位,且不能出现空格'
-        ],
-        repwd: function(value) {
-            var pwd = $('.reg-box [name=password]').val()
-            if (pwd !== value) {
-                return '两次密码不一致!'
+            pwd: [/^[\S]{6,12}$/,
+                '密码必须6到12位,且不能出现空格'
+            ],
+            repwd: function(value) {
+                var pwd = $('.reg-box [name=password]').val()
+                if (pwd !== value) {
+                    return '两次密码不一致!'
+                }
             }
-        }
-    })
-
+        })
+        //http://api-breakingnews-web.itheima.net
+    var data = {
+        username: $('#form_reg [name=username]').val(),
+        password: $('#form_reg [name=password]').val()
+    }
     $('#form_reg').on('submit', function(e) {
         e.preventDefault()
-        $.post('http://api-breakingnews-web.itheima.net', {
-            username: $('#form_reg [name=username]').val(),
-            password: $('#form_reg [name=password]').val()
-        }, function(res) {
+        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function(res) {
             if (res.status !== 0) {
-                return console.log(res.message)
+                return layer.msg(res.message)
             }
-            console.log('注册成功！')
+            layer.msg('注册成功，请登录！')
+
+            $('#link_login').click()
+
+        })
+    })
+
+    $('#form_login').submit(function(e) {
+        e.preventDefault()
+        $.ajax({
+            url: 'http://ajax.frontend.itheima.net/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    console.log(res.msg);
+                    return layer.msg('登录失败')
+                }
+                layer.msg('登录成功！')
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
+            }
         })
     })
 })
